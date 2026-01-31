30585fdeac08c634284085a369df89b2bd4cb3c3
uhhbads <abadshoodee@gmail.com>
2026-01-31 22:39:44 +0800

    init

---
+++ src/main/java/com/practice/sprintone_coffeeshop/SprintoneCoffeeshopApplication.java
@@ -0,0 +1,13 @@
+package com.practice.sprintone_coffeeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintoneCoffeeshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintoneCoffeeshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/sprintone_coffeeshop/SprintoneCoffeeshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.sprintone_coffeeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintoneCoffeeshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
